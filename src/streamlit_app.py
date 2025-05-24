56b0bf2cb6f1ffab430df018949253b70c128774
Abhinav-Singh-Emma <abhinav.singh@emma-sleep.com>
2025-05-24 10:52:14 +0200

    delete other app

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(page_title='mehul gupta\'s portfolio' ,layout="wide",page_icon='👨‍🔬')
+
+st.sidebar.markdown(info['Stackoverflow_flair'],unsafe_allow_html=True)
+
 st.title("My Streamlit App")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.area_chart(chart_data)
+
